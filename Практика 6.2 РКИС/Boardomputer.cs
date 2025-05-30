6764200b8fd362c4e5a99b7df5e2b2e09f88b89a
MaxUnLuck <ggxuesos41@gmail.com>
2025-05-30 07:21:55 +0300

    Add files via upload

---
+++ Практика 6.2 РКИС/Boardomputer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практика_6._2_РКИС
+{
+    internal class BoardComputer
+    {
+        private readonly Action<String> action;
+        public BoardComputer(Action<String> _action)
+        {
+            action = _action;
+        }
+        public bool StartDiagnostic()
+        {
+            Random random = new Random();
+            int eventN = random.Next(1, 3);
+            switch (eventN)
+            {
+                case 1:
+                    action("Заглохли");
+                    return false;
+                case 2:
+                    action("Всё отлично!");
+                    return true;
+                default:
+                    action("Неизвестная ошибка");
+                    return false;
+            }
+        }
+    }
+}
